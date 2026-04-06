7305168c93f6bb758ebc05f1406af1d7e2da9896
Paschalolo <pastechnology1@gmail.com>
2026-04-06 13:51:56 -0700

    mesa

--- test1.s
+++ test1.s
@@ -4,24 +4,26 @@
 # 1 "/usr/include/stdc-predef.h" 1 3 4
 # 0 "<command-line>" 2
 # 1 "src/test1.S"
-; Hello world in assembly
 
-; Author : Pascahl Ahanmisi
-; Purpose teesting script on mac
+
+
+
+
 .data
  helloworld : .ascii "Hello world\n My name is Paschal\n"
 
+.text
 .global _start
 
-_start :
- mov x0, #1
- ldr x1 , helloworld
- mov x2 , #32
- mov x8 , 0X40
+_start:
 
- svc 0; call syscall
+    mov x0, #1
+    ldr x1, =helloworld
+    mov x2, #32
+    mov x8, #64
+    svc 0
 
- ; terminate the process '
- mov x0 , 0x5d
- mov x1 , #0
- svc 0
+
+    mov x0, #0
+    mov x8, #93
+    svc 0

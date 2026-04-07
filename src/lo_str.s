3f8bc21e8728acf15fc397e77b214208f337a2de
Paschalolo <pastechnology1@gmail.com>
2026-04-07 12:55:49 -0700

    mesa

--- src/lo_str.s
+++ src/lo_str.s
@@ -6,3 +6,22 @@
 .data 
 .balign 4 
 snu: .word 0x12345578
+
+.text 
+.global _start 
+_start : 
+	mov x4 , #0 
+	ldr  x1 , =snu // load address of snu here 
+	ldrb w0 , [x1 , x4 ]
+	add x4 , x4 , #1
+	ldrb w0 , [x1 , x4 ]
+	add x4 , x4 , #1
+	ldrb w0 , [x1 , x4 ]
+	add x4 , x4 , #1
+	ldrb w0 , [x1 , x4 ]
+
+	// syscall to end 
+
+	mov x8 , #93 
+	mov x0 , #0 
+	svc #0
